8aa1026ffed1bc562950721c1cf87969b9c3130b
Thomas Ojeda <thomasojeda05@gmail.com>
2019-11-30 00:51:33 -0300

    Nada

--- Individual.h
+++ Individual.h
@@ -7,55 +7,51 @@
 #include <string>
 #include "Individual.h"
 #include "time.h"
-#inclyde "Vector.h"
+
 using namespace std;
 class Individual {
     string genePool='0123';
 public:
-    Vector weight[8]={0,0,0,0,0,0,0,0};
-    string chromosome;
-    int fitness;
+    vector weight[8]={0,0,0,0,0,0,0,0};
+    string chromosome="";
+    int fitness=0;
+
+    Individual();
     Individual(string chromosome);
+    Individual(string chromosome,vector weight);
     Individual mate(Individual parent2);
-    int cal_fitness();
     bool operator<(const Individual &ind1, const Individual &ind2);
     char getMutatedGene(int geneNumber);
     static int random_num(int start, int end);
+    int calFitness();
+    void updateWeights();
 };
 
 Individual::Individual(string chromosome)
 {
     this->chromosome = chromosome;
-    fitness = cal_fitness();
     for(int i = 0;i<8;i++)
     {
-        gnome += getMutatedGene(i);
-        weight.add(0);
+        weight.push_back(0);
     }
 };
 
 Individual::Individual()
 {
-    this->chromosome=Chromosome c;
-    string gnome = "";
+    string newcrom = "";
     for(int i = 0;i<8;i++)
     {
-        gnome += getMutatedGene(i);
-        weight.add(0);
+        newcrom += getMutatedGene(i);
+        weight.push_back(0);
     }
-
+    chromosome=newcrom;
 };
 
-Individual::Individual(string chromosome,Vector<int> weight)
+Individual::Individual(string chromosome,vector<int> weight)
 {
     this->weight=weight;
     this->chromosome = chromosome;
-    fitness = cal_fitness();
-    for(int i = 0;i<8;i++)
-    {
-        gnome += getMutatedGene(i);
-        weight.push_back(weight.get(i));
-    }
+    fitness = calFitness();
 };
 
 // Perform mating and produce new offspring
@@ -63,7 +59,8 @@ Individual Individual::mate(Individual par2)
 {
     // chromosome for offspring
     string child_chromosome = "";
-    Vector<int> child_weight;
+    vector<int> child_weight;
+    child_weight.clear();
     for(int i = 0;i<8;i++)
     {
         // random probability
@@ -72,13 +69,17 @@ Individual Individual::mate(Individual par2)
         // if prob is less than 0.50, insert gene
         // from parent 1
         if(p < 0.5)
+        {
             child_chromosome += chromosome[i];
-            child_weight.push_back(weight.get(i));
+            child_weight.push_back(weight.at(i));
+        }
             // if prob is between 0.50, insert gene
             // gene from parent 2
         else
+        {
             child_chromosome += par2.chromosome[i];
             child_weight.push_back(par2.weight.get(i));
+        }
             // insert random genes(mutate),
             // for maintaining diversity
             for (int i=0;i<8;i++)
@@ -113,15 +114,27 @@ char Individual::getMutatedGene(int geneNumber) //Hay que cambiar esta funcion p
 
 static int Individual::random_num(int start, int end)
 {
-int range = (end-start)+1;
-int random_int = start+(rand()%range);
-return random_int;
+    srand(time(null));
+    int range = (end-start)+1;
+    int random_int = start+(rand()%range);
+    return random_int;
 }
 
-string Individual::create_gnome()
-{
-
+int Individual::calFitness() {
+    int sum=0;
+    for (int i=0;i<8;i++)
+    {
+        sum=sum+weight.at(i);
+    }
+    return sum;
 }
 
+void Individual::updateWeights(Individual target) {
+    for (int i=0;i<8;i++)
+    {
+        if (chromosome[i]==target.chromosome.[i])
+            weight[i]++;
+    }
+}
 
 #endif //EXPLORATORIAV2_INDIVIDUAL_H
--- main.cpp
+++ main.cpp
@@ -7,80 +7,60 @@ using namespace std;
 // Number of individuals in each generation
 #define POPULATION_SIZE 100
 
-// Valid Genes
-const string GENES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOP"\
-"QRSTUVWXYZ 1234567890, .-;:_!\"#%&/()=?@${[]}";
-
-// Target string to be generated
-string TARGET = "default target";
-
-// create chromosome or string of genes
-
-
 // Driver code
 int main(int argc,char * argv[])
 {
-    string t=argv[1];
-    TARGET=t;
-    srand((unsigned)(time(0)));
-
+    string target="null";
     // current generation
     int generation = 0;
-
     vector<Individual> population;
     bool found = false;
-
     // create initial population
     for(int i = 0;i<POPULATION_SIZE;i++)
     {
-        Individual i;
-        population.push_back(i);
+        Individual ind;
+        population.push_back(ind);
     }
 
     while(! found)
     {
+        Individual target=///ESCRIBIR CODIGO PARA OBTENER NUEVO TARGET
+
+        //Update weights of each individual
+
+        for (int i=0;i<POPULATION_SIZE;i++) {
+            population[i].updateWeights(target);
+        }
+
         // sort the population in increasing order of fitness score
         sort(population.begin(), population.end());
-
-        // if the individual having lowest fitness score ie.
-        // 0 then we know that we have reached to the target
-        // and break the loop
-        if(population[0].fitness <= 0)
-        {
-            found = true;
-            break;
-        }
 
         // Otherwise generate new offsprings for new generation
         vector<Individual> new_generation;
 
         // Perform Elitism, that mean 10% of fittest population
         // goes to the next generation
-        int s = (10*POPULATION_SIZE)/100;
+        int s = 0.1*POPULATION_SIZE;
         for(int i = 0;i<s;i++)
             new_generation.push_back(population[i]);
 
         // From 50% of fittest population, Individuals
         // will mate to produce offspring
-        s = (90*POPULATION_SIZE)/100;
+        s = POPULATION_SIZE-s;
         for(int i = 0;i<s;i++)
         {
-            int len = population.size();
-            Individual::random_num(0, 50);
-            Individual parent1 = population[r];
-            r = random_num(0, 50);
-            Individual parent2 = population[r];
+            int r = Individual::random_num(0, POPULATION_SIZE*0.5);
+            Individual parent1 = population.at(r);
+            r = Individual::random_num(0, POPULATION_SIZE*0.5);
+            Individual parent2 = population.at(r);
             Individual offspring = parent1.mate(parent2);
             new_generation.push_back(offspring);
         }
         population = new_generation;
         cout<< "Generation: " << generation << "\t";
-        cout<< "String: "<< population[0].chromosome <<"\t";
-        cout<< "Fitness: "<< population[0].fitness << "\n";
+        cout<< "Best individual: "<< population.at(0).chromosome <<"\t";
+        cout<< "Fitness: "<< population.at(0).fitness << "\n";
 
         generation++;
     }
-    cout<< "Generation: " << generation << "\t";
-    cout<< "String: "<< population[0].chromosome <<"\t";
-    cout<< "Fitness: "<< population[0].fitness << "\n";
 }
